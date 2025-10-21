5808cc762ed70bc09a56005a771a28c45e3212b1
Roxana Rolon <roxdev2023@gmail.com>
2025-10-21 17:05:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6832356612-15236",
+  "appId": "1:968723589909:web:1c0b6f9554a3b10406d71c",
+  "apiKey": "AIzaSyBVCDQLjJeQjM4qv6f8JjjYnECI8n-Fbgs",
+  "authDomain": "studio-6832356612-15236.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "968723589909"
+};
